caf4b35852008b65f3e66082f799914e7ad9c6a4
ZougaRida <96395950+R-zouga@users.noreply.github.com>
2024-12-26 17:00:34 +0100

    Added tests for the transition problem solution

---
+++ tests/test_transition_point.py
@@ -0,0 +1,59 @@
+from unittest import TestCase
+from transition_point import transition_point
+
+
+class TestTransitionPoint(TestCase):
+    def test_normal_transition_point(self):
+        array = [0, 0, 0, 1, 1, 1]
+        self.assertEqual(transition_point(array), 3)
+
+        array = [0, 0, 0, 0, 0, 1, 1, 1]
+        self.assertEqual(transition_point(array), 5)
+
+        array = [0, 1, 1, 1, 1]
+        self.assertEqual(transition_point(array), 1)
+
+        array = [0, 0, 0, 0, 1]
+        self.assertEqual(transition_point(array), 4)
+
+        array = [0] * 20 + [1]
+        self.assertEqual(transition_point(array), 20)
+
+        array = [0] + [1] * 20
+        self.assertEqual(transition_point(array), 1)
+
+        array = [0] * 15 + [1] * 100
+        self.assertEqual(transition_point(array), 15)
+
+
+    def test_all_ones(self):
+        array = [1, 1, 1, 1]
+        self.assertEqual(transition_point(array), 0)
+
+        array = [1, 1, 1, 1, 1]
+        self.assertEqual(transition_point(array), 0)
+
+        array = [1] * 50
+        self.assertEqual(transition_point(array), 0)
+
+        array = [1]
+        self.assertEqual(transition_point(array), 0)
+
+    def test_all_zeros(self):
+        array = [0, 0, 0, 0, 0]
+        self.assertEqual(transition_point(array), -1)
+
+        array = [0, 0]
+        self.assertEqual(transition_point(array), -1)
+
+        array = [0] * 50
+        self.assertEqual(transition_point(array), -1)
+
+        array = [0]
+        self.assertEqual(transition_point(array), -1)
+
+
+
+
+
+
