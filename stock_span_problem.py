17de53fedecf999abe8b33f8b84b857824042b99
ZougaRida <96395950+R-zouga@users.noreply.github.com>
2025-01-05 01:24:09 +0100

    Solved the stock span problem with medium difficulty and added some tests to it

---
+++ stock_span_problem.py
@@ -0,0 +1,58 @@
+def stock_span_problem(arr: list[int]) -> list[int]:
+    """
+    This function consists on finding in simpler terms the first max from left of each arr[i]
+    or rather getting back its index, of course if it exists.
+    I admit this was by far the solution I was able to get at first which is very straightforward to implement.
+    once understood that we need to get the first max from left
+
+    Time Complexity: O(n^2) and Memory Complexity: O(1)
+
+    https://www.geeksforgeeks.org/problems/stock-span-problem-1587115621/1
+    """
+    n = len(arr)
+    first_max_from_left = [1]
+    for i in range(1, n):
+        for j in range(1, i + 1):
+            if arr[i - j] > arr[i]:
+                first_max_from_left.append(j)
+                break
+
+            if i - j == 0:
+                first_max_from_left.append(j + 1)
+    return first_max_from_left
+
+
+def stock_span_problem_better_solution(arr: list[int]) -> list[int]:
+    """
+    This method is honestly really powerful, had maybe some initial guess to how it goes but definitely
+    didn't come with its solution, the idea is to create a temporary stack, a special monotonic decreasing stack
+    that keeps track of the latest values to which we counted the span, so that next element that comes, either we
+    use that and pop them out and keep track of their values in the new appended indeed, or keep them there and append
+    the min value along with them to
+    REMEMBER WELL THIS USEFUL TECHNIQUE!
+    
+
+
+    """
+    n = len(arr)
+    result = []
+    stack = []
+
+    for i in range(n):
+
+        while stack and arr[stack[-1]] <= arr[i]:
+            stack.pop()
+
+        if stack:
+            result.append(i - stack[-1])
+        else:
+            result.append(i + 1)
+
+        stack.append(i)
+    return result
+
+
+if __name__ == "__main__":
+    array = [int(x) for x in input().split()]
+    print(stock_span_problem(array))
+    print(stock_span_problem_better_solution(array))
---
+++ tests/test_stock_span_problem.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+
+from stock_span_problem import stock_span_problem, stock_span_problem_better_solution
+
+
+class TestStockSpanProblem(TestCase):
+    def test_single_element_array(self):
+        array = [42]
+        self.assertEqual(stock_span_problem(array), [1])
+        self.assertEqual(stock_span_problem_better_solution(array), [1])
+
+        array = [1]
+        self.assertEqual(stock_span_problem(array), stock_span_problem_better_solution(array))
+
+    def test_increasing_order(self):
+        array = [10, 20, 30, 40, 50]
+        self.assertEqual(stock_span_problem(array), [1, 2, 3, 4, 5])
+        self.assertEqual(stock_span_problem_better_solution(array), [1, 2, 3, 4, 5])
+
+        array = [60, 80, 130, 150, 200, 400, 500, 501]
+        self.assertListEqual(stock_span_problem(array), stock_span_problem_better_solution(array))
+
+    def test_decreasing_order(self):
+        array = [50, 40, 30, 20, 10]
+        self.assertEqual(stock_span_problem(array), [1, 1, 1, 1, 1])
+        self.assertEqual(stock_span_problem_better_solution(array), [1, 1, 1, 1, 1])
+
+        array = [100, 96, 90, 89, 88, 71, 62]
+        self.assertEqual(stock_span_problem(array), [1, 1, 1, 1, 1, 1, 1])
+        self.assertEqual(stock_span_problem_better_solution(array), [1, 1, 1, 1, 1, 1, 1])
+
+    def test_random_array(self):
+        array = [10, 4, 5, 90, 120, 80]
+        self.assertEqual(stock_span_problem(array), [1, 1, 2, 4, 5, 1])
+        self.assertEqual(stock_span_problem_better_solution(array), [1, 1, 2, 4, 5, 1])
+
+        array = [100, 80, 60, 70, 60, 75, 85]
+        self.assertEqual(stock_span_problem(array), [1, 1, 1, 2, 1, 4, 6])
+        self.assertEqual(stock_span_problem_better_solution(array), [1, 1, 1, 2, 1, 4, 6])
+
+        array = [10, 4, 4, 90, 120, 80]
+        self.assertEqual(stock_span_problem(array), [1, 1, 2, 4, 5, 1])
+        self.assertEqual(stock_span_problem_better_solution(array), [1, 1, 2, 4, 5, 1])
+
+    def test_all_elements_same(self):
+        array = [7, 7, 7, 7]
+        self.assertEqual(stock_span_problem(array), [1, 2, 3, 4])
+        self.assertEqual(stock_span_problem_better_solution(array), [1, 2, 3, 4])
