bbe6f0e11b8f6dcadac2f6e49a9e9a180fc629ae
ZougaRida <96395950+ZougaRida@users.noreply.github.com>
2026-01-29 12:12:09 +0100

    modify the tests for the maximum_profit function

--- arrays/tests/test_buy_and_sell_one_transaction.py
+++ arrays/tests/test_buy_and_sell_one_transaction.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from buy_and_sell_one_transaction import maximum_profit
+from arrays.buy_and_sell_one_transaction import maximum_profit
 
 
 class TestTransaction(TestCase):
@@ -8,11 +8,16 @@ class TestTransaction(TestCase):
         array = [7, 10, 1, 3, 6, 9, 2]
         self.assertEqual(maximum_profit(array), 8)
 
+        # here the prices array is in decreasing order
+        # so absolutely no profit can be made
         array = [7, 6, 4, 3, 1]
         self.assertEqual(maximum_profit(array), 0)
 
         array = [7, 6, 4, 3, 1, 1000]
         self.assertEqual(maximum_profit(array), 999)
+
+        array = [13, 2, 1, 9, 3, 4, 16]
+        self.assertEqual(maximum_profit(array), 15)
 
         array = list(range(100))
         self.assertEqual(maximum_profit(array), 99)
@@ -22,4 +27,7 @@ class TestTransaction(TestCase):
         self.assertEqual(maximum_profit(array), 0)
 
         array = [32]
+        self.assertEqual(maximum_profit(array), 0)
+
+        array = [0]
         self.assertEqual(maximum_profit(array), 0)
